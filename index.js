061a7df6b3aaa531e8166f51dc935e5e0e40bf76
mastero101 <mastershift_101@hotmail.com>
2023-11-21 13:43:46 -0600

    Agregado un ciclo infinito para la escucha

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const FormData = require('form-data');
 require('dotenv').config();
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-let openAiResponse;
 
 // Configuración de la grabación
 const recordingOptions = {
@@ -17,49 +16,60 @@ const recordingOptions = {
   debug: true,
 };
 
-// Nombre del archivo WAV para almacenar la grabación
-const wavFileName = 'audio_input.wav';
-
-// Crear un objeto de escritura para el archivo WAV
-const fileWriter = new wav.FileWriter(wavFileName, {
-  channels: recordingOptions.channels,
-  sampleRate: recordingOptions.rate,
-  bitDepth: 32,
-});
-
-// Crear un objeto de entrada de micrófono
-const microphone = mic(recordingOptions);
-
-// Configurar la transmisión de micrófono al archivo WAV
-const micInputStream = microphone.getAudioStream();
-micInputStream.pipe(fileWriter);
-
-console.log('Grabando audio durante 10 segundos...');
-
-// Manejar eventos de finalización de la grabación
-micInputStream.on('error', (error) => {
-  console.error('Error en la transmisión de audio:', error);
-});
-
-// Iniciar la grabación
-microphone.start();
-
 // Detener la grabación después de 5 segundos
-setTimeout(async () => {
-  console.log('Deteniendo la grabación...');
-  microphone.stop();
-  fileWriter.end();
-
-  // Enviar el archivo de audio a OpenAI
-  await sendAudioToOpenAI(wavFileName);
-  // Enviar texto a OpenAI y obtener la respuesta
+async function main() {
   try {
-    openAiResponse = await sendTextToOpenAI(text);
-    console.log('GPT:', openAiResponse);
+    // Nombre del archivo WAV para almacenar la grabación
+    const wavFileName = 'audio_input.wav';
+
+    // Crear un objeto de escritura para el archivo WAV
+    const fileWriter = new wav.FileWriter(wavFileName, {
+      channels: recordingOptions.channels,
+      sampleRate: recordingOptions.rate,
+      bitDepth: 32,
+    });
+
+    // Crear un objeto de entrada de micrófono
+    const microphone = mic(recordingOptions);
+
+    // Configurar la transmisión de micrófono al archivo WAV
+    const micInputStream = microphone.getAudioStream();
+    micInputStream.pipe(fileWriter);
+
+    // Manejar eventos de finalización de la grabación
+    micInputStream.on('error', (error) => {
+      console.error('Error en la transmisión de audio:', error);
+    });
+    console.log('Grabando audio durante 10 segundos...');
+    microphone.start();
+
+    // Grabar durante 10 segundos
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // Detener la grabación
+    console.log('Deteniendo la grabación...');
+    microphone.stop();
+    fileWriter.end();
+
+    // Enviar el archivo de audio a OpenAI
+    await sendAudioToOpenAI(wavFileName);
+
+    // Enviar texto a OpenAI y obtener la respuesta
+    try {
+      const openAiResponse = await sendTextToOpenAI(text);
+      console.log('GPT:', openAiResponse);
+      console.log('------------------------------------------------');
+    } catch (error) {
+      console.error('Error al obtener respuesta de OpenAI:', error.message);
+    }
   } catch (error) {
-    console.error('Error al obtener respuesta de OpenAI:', error.message);
+    console.error('Error al manejar la grabación:', error);
+  } finally {
+    // Reiniciar el script después de un breve retraso
+    console.log('Reiniciando el script...');
+    setTimeout(main, 2000);
   }
-}, 10000);
+}
 
 // Función para enviar datos de audio a OpenAI
 async function sendAudioToOpenAI(audioFilePath) {
@@ -103,3 +113,6 @@ async function sendTextToOpenAI(text) {
     throw error;
   }
 }
+
+// Iniciar la función principal
+main();
